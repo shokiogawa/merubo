471a89b6fb3f4d385d41459a7b8d68cb80aa81e0
shokiogawa <shouki2578@gmail.com>
2022-12-03 20:57:46 +0900

    add: 名前解決

--- lib/screen/message_bord_preview_screen/main.dart
+++ lib/screen/message_bord_preview_screen/main.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:merubo/model/entity/message_bord.dart';
-import 'package:merubo/screen/preview_screen/preview_message_bord_bottom.dart';
-import 'package:merubo/screen/preview_screen/preview_message_bord_message_area.dart';
-import 'package:merubo/screen/preview_screen/preview_message_bord_top.dart';
+import 'package:merubo/screen/message_bord_preview_screen/preview_message_bord_bottom.dart';
+import 'package:merubo/screen/message_bord_preview_screen/preview_message_bord_message_area.dart';
+import 'package:merubo/screen/message_bord_preview_screen/preview_message_bord_top.dart';
 
 class PreviewMessageBordScreen extends StatelessWidget {
   const PreviewMessageBordScreen({Key? key}) : super(key: key);
