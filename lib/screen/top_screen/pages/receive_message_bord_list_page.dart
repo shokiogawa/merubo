ad26da5668a981bc5e2ad59a1c2855eecb4166e6
shokiogawa <shouki2578@gmail.com>
2022-12-30 11:27:24 +0900

    add:　form入力

--- lib/screen/top_screen/pages/receive_message_bord_list_page.dart
+++ lib/screen/top_screen/pages/receive_message_bord_list_page.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:merubo/provider/message_bord_provider.dart';
+import 'package:merubo/provider/register_message_bord_provider.dart';
 import 'package:merubo/screen/top_screen/widget/message_bord_by_year.dart';
+import 'package:merubo/widgets/button.dart';
 
 class ReceiveMessageBordList extends ConsumerWidget {
   const ReceiveMessageBordList({Key? key}) : super(key: key);
@@ -42,21 +44,7 @@ class ReceiveMessageBordList extends ConsumerWidget {
                                             isScrollControlled: true,
                                             context: context,
                                             builder: (BuildContext context) {
-                                              return Scaffold(
-                                                body: Column(
-                                                  children: [
-                                                    const SizedBox(height: 50),
-                                                    IconButton(
-                                                        onPressed: () {
-                                                          Navigator.of(context)
-                                                              .pop();
-                                                        },
-                                                        icon: const Icon(
-                                                            Icons.close)),
-
-                                                  ],
-                                                ),
-                                              );
+                                              return const AddMessageBord();
                                             });
                                       },
                                       icon: const Icon(Icons.add))),
@@ -91,5 +79,209 @@ class ReceiveMessageBordList extends ConsumerWidget {
         loading: () => const Center(
               child: CircularProgressIndicator(),
             ));
+  }
+}
+
+enum MessageBordType {
+  merubo("Merubo"),
+  online("オンライン"),
+  paper("紙");
+
+  final String displayName;
+
+  const MessageBordType(this.displayName);
+}
+
+final selectedMessageBordType = StateProvider((ref) => MessageBordType.merubo);
+
+class AddMessageBord extends StatelessWidget {
+  const AddMessageBord({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const SizedBox(height: 50),
+            IconButton(
+                onPressed: () {
+                  Navigator.of(context).pop();
+                },
+                icon: const Icon(Icons.close)),
+            Column(
+              children: [
+                const Text("どの寄せ書きえお受け取りましたか?"),
+                Consumer(builder: (BuildContext context, WidgetRef ref, _) {
+                  final messageBordType = ref.watch(selectedMessageBordType);
+                  return Column(
+                    children: [
+                      Column(
+                        children: [
+                          ListTile(
+                            title: Text(MessageBordType.merubo.displayName),
+                            leading: Radio(
+                                value: MessageBordType.merubo,
+                                groupValue: messageBordType,
+                                onChanged: (value) {
+                                  ref
+                                      .read(selectedMessageBordType.notifier)
+                                      .update((state) => value!);
+                                }),
+                          ),
+                          ListTile(
+                            title: Text(MessageBordType.online.displayName),
+                            leading: Radio(
+                                value: MessageBordType.online,
+                                groupValue: messageBordType,
+                                onChanged: (value) {
+                                  ref
+                                      .read(selectedMessageBordType.notifier)
+                                      .update((state) => value!);
+                                }),
+                          ),
+                          ListTile(
+                            title: Text(MessageBordType.paper.displayName),
+                            leading: Radio(
+                                value: MessageBordType.paper,
+                                groupValue: messageBordType,
+                                onChanged: (value) {
+                                  ref
+                                      .read(selectedMessageBordType.notifier)
+                                      .update((state) => value!);
+                                }),
+                          ),
+                        ],
+                      ),
+                      const SizedBox(height: 20),
+                      // 追加ページ
+                      Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 20),
+                        child: messageBordType == MessageBordType.merubo
+                            ? const AddMeruboMessageBord()
+                            : messageBordType == MessageBordType.online
+                                ? const AddOnlineMessageBord()
+                                : const Text("Paper"),
+                      )
+                    ],
+                  );
+                }),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class AddOnlineMessageBord extends StatelessWidget {
+  const AddOnlineMessageBord({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Form(
+            child: Column(
+              children: [
+                TextFormField(
+                  decoration: const InputDecoration(
+                      border: OutlineInputBorder(),
+                      hintStyle: TextStyle(fontSize: 12),
+                      contentPadding: EdgeInsets.all(10),
+                      hintText: "受け取ったURLを入力してください"),
+                ),
+                const SizedBox(height: 20),
+                TextFormField(
+                  decoration: const InputDecoration(
+                      border: OutlineInputBorder(),
+                      hintStyle: TextStyle(fontSize: 12),
+                      contentPadding: EdgeInsets.all(10),
+                      hintText: "誰から受け取りましたか？"),
+                ),
+                const SizedBox(height: 20),
+                TextFormField(
+                  decoration: const InputDecoration(
+                      border: OutlineInputBorder(),
+                      hintStyle: TextStyle(fontSize: 12),
+                      contentPadding: EdgeInsets.all(10),
+                      hintText: "なんのお祝いですか？"),
+                ),
+                const SizedBox(height: 20),
+                TextFormField(
+                  decoration: const InputDecoration(
+                      border: OutlineInputBorder(),
+                      hintStyle: TextStyle(fontSize: 12),
+                      contentPadding: EdgeInsets.all(10),
+                      hintText: "いつ受け取りましたか？"),
+                ),
+                const SizedBox(height: 20),
+              ],
+            )
+        ),
+        Button(text: "登録", buttonColor: Colors.orangeAccent, onPressed: (){})
+      ],
+    );
+  }
+}
+
+class AddMeruboMessageBord extends ConsumerWidget {
+  const AddMeruboMessageBord({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final controller =
+        ref.watch(registerMessageBordProvider).messageBordIdController;
+    return Column(
+      children: [
+        Form(
+            child: TextFormField(
+          controller: controller,
+          decoration: const InputDecoration(
+              border: OutlineInputBorder(),
+              hintStyle: TextStyle(fontSize: 12),
+              contentPadding: EdgeInsets.all(10),
+              hintText: "寄せ書きIDを入力してください"),
+        )),
+        const SizedBox(height: 20),
+        Button(
+            text: "受け取る",
+            buttonColor: Colors.orangeAccent,
+            onPressed: () async {
+              // ローディング開始
+              showGeneralDialog(
+                  barrierDismissible: false,
+                  context: context,
+                  pageBuilder: (context, animation1, animation2) {
+                    return const Center(child: CircularProgressIndicator());
+                  });
+              showDialog(
+                  context: context,
+                  builder: (_) {
+                    return AlertDialog(
+                      title: const Text(
+                        "取得に失敗しました",
+                        style: TextStyle(fontSize: 17),
+                      ),
+                      titlePadding: const EdgeInsets.symmetric(
+                          horizontal: 20, vertical: 20),
+                      actions: [
+                        GestureDetector(
+                          child: const Padding(
+                            padding: EdgeInsets.all(8.0),
+                            child: Text("戻る"),
+                          ),
+                          onTap: () {
+                            Navigator.of(context).pop();
+                          },
+                        )
+                      ],
+                    );
+                  });
+            })
+      ],
+    );
   }
 }
